55b3605294b2f3a3f7e8007c760d0009992fec7c
Troy Yauk <troyyauk@gmail.com>
2025-12-02 14:53:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,43 @@
 let library;
 const main_container = document.getElementsByClassName("main");
+let movieItems;
+let setGenre = localStorage.getItem('the_genre');
+const the_menu = document.getElementsByClassName("menu");
+const menu_icon = document.getElementsByClassName("control-wrapper");
+let menu_toggle = false;
+let menuItems;
+let data;
+
+function menu_01(){
+	toggle_menu();
+}
+
+function menu_02(){
+	toggle_menu();
+}
+
+function toggle_menu(){
+	if(menu_toggle == true){
+		the_menu[0].style.top = "-72vh";
+		menu_icon[0].style.transform = "rotate(0deg)";
+		menu_icon[1].style.transform = "rotate(0deg)";
+		menu_toggle = false;
+	}else{
+		the_menu[0].style.top = "187px";
+		menu_icon[0].style.transform = "rotate(180deg)";
+		menu_icon[1].style.transform = "rotate(180deg)";
+		menu_toggle = true;
+	}
+}
 
 function load_library(){
 
+	if(setGenre == null || setGenre == "All Genres"){
+		setGenre = "all";
+	}
+
+	document.getElementById("current-list").innerHTML = setGenre + " Movies";
+	
 	getMeta();
 
 }
@@ -14,8 +49,15 @@ async function getMeta(){
 
   	const response = await fetch(request);
   	const movies = await response.json();
+  	data = movies.movies;
 
-  	library = movies.movies;
+  	if(setGenre == "all"){
+  		library = data;
+  	}else{
+  		library = data.filter(movies => movies.genre && movies.genre.includes(setGenre));
+  	}
+
+  	localStorage.clear();
 
   	populateLibrary();
 
@@ -23,19 +65,44 @@ async function getMeta(){
 
 function populateLibrary(){
 
-	console.log(library);
-
 	for(let i = 0;i < library.length;i++){
 
 		const movieItem = document.createElement("div");
-		movieItem.classList.add = 'item-wrapper';
-		movieItem.innerHTML = "<div class='movie-item'><span class='title'>" + library[i].title + "<br><span class='year'>(" + library[i].year + ")</span></span></div>";
+		movieItem.classList.add('item-wrapper');
+		if((library[i].genre).length > 1){
+			movieItem.innerHTML = "<div class='movie-item'><span class='title'>" + library[i].title + "<br><span class='year'>(" + library[i].year + ")</span></span></div><div class='movie-details'><span class='title'>" + library[i].title + " (" + library[i].year + ")</span><span class='director'>Directed By: " + library[i].director + "</span><p class='description'>" + library[i].description + "</p><span class='genre'>" + library[i].genre[0] + "</span><span class='genre'>" + library[i].genre[1] + "</span></div>";
+		}else{
+			movieItem.innerHTML = "<div class='movie-item'><span class='title'>" + library[i].title + "<br><span class='year'>(" + library[i].year + ")</span></span></div><div class='movie-details'><span class='title'>" + library[i].title + " (" + library[i].year + ")</span><span class='director'>Directed By: " + library[i].director + "</span><p class='description'>" + library[i].description + "</p><span class='genre'>" + library[i].genre[0] + "</span></div>";
+		}
 		main_container[0].appendChild(movieItem);
 		setTimeout(function() {
 			const imageItem = document.getElementsByClassName('movie-item');
 			imageItem[i].style.backgroundImage = "url('" + library[i].previewImage + "')";
 		}, 10);
 
-	} 
+	}
 
 }
+
+setTimeout(function() {
+	movieItems = document.querySelectorAll('.item-wrapper');
+	movieItems.forEach(element => {
+		element.addEventListener('click', function(event) {
+			for(let i = 0;i < movieItems.length;i++){
+				movieItems[i].classList.remove("selected");
+			}
+    		this.classList.add("selected");
+  		});
+	});
+}, 100);
+
+setTimeout(function() {
+	menuItems = document.querySelectorAll('.menu-item');
+	menuItems.forEach(element => {
+		element.addEventListener('click', function(event) {
+			const genre = this.innerHTML;
+			localStorage.setItem('the_genre', genre);
+			window.location.reload();
+		});
+	});
+}, 10);
