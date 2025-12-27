89f01ad19d779c2ee28a54b5a293a42c18b43b68
Troy Yauk <troyyauk@gmail.com>
2025-12-27 17:22:21 -0500

    Create release-year.js

---
+++ library/release-year.js
@@ -0,0 +1,33 @@
+const menuItems = document.getElementsByClassName("menu-item");
+let year;
+
+window.onload = function() {
+
+	for(let i = 0;i < menuItems.length;i++){
+		if(i % 2 === 0){
+			menuItems[i].classList.add("alt");
+		}
+	}
+
+	for(let i = 0;i < menuItems.length;i++){
+		setTimeout(function(){
+			menuItems[i].style.transform = "translateX(0px)";
+		}, i * 100);
+	}
+
+};
+
+
+yearItem = document.querySelectorAll('.menu-item');
+yearItem.forEach(element => {
+	element.addEventListener('click', function(event) {
+		year = this.innerHTML;
+		localStorage.setItem("timeframe", year);
+		if(year !== "2025"){
+			year = year.slice(0,3);
+		}
+		console.log(year);
+		localStorage.setItem('the_filter', year);
+		window.location.href = "index.html";
+  	});
+});
