4f0190b58a01eb5ffaf7e5207e34e88ff9cad45a
Troy Yauk <troyyauk@gmail.com>
2026-01-01 14:39:12 -0500

    Create main.js

---
+++ global/scripts/main.js
@@ -0,0 +1,110 @@
+let library;
+let data;
+let the_id = "0";
+let filter = localStorage.getItem('the_filter');
+const clearB = document.getElementsByClassName("clear-button");
+const logo = document.getElementsByClassName("logo");
+let logoOffset = 100;
+
+if (filter == null){
+	filter = "all";
+}
+
+const container = document.getElementsByClassName("library-container");
+
+
+function initialize() {
+
+
+	the_library();
+}
+
+
+async function the_library(){
+
+	const requestURL = "https://t-yauk.github.io/peak-theatre/library.json";
+  	const request = new Request(requestURL);
+
+  	const response = await fetch(request);
+  	const rawJSON = await response.json();
+
+  	if(filter == "all"){
+  		library = rawJSON.movies;
+  		document.getElementById("the-title").innerHTML = "All Movies";
+  		clearB[0].style.display = "none";
+  	}else if((filter.includes("19") == true) || (filter.includes("20") == true)){
+  		data = rawJSON.movies;
+  		library = data.filter(movies => movies.year && movies.year.includes(filter));
+  		document.getElementById("the-title").innerHTML = localStorage.getItem("timeframe");
+  	}else{
+  		data = rawJSON.movies;
+  		library = data.filter(movies => movies.genre && movies.genre.includes(filter));
+  		document.getElementById("the-title").innerHTML = filter + " Movies";
+  	}
+
+  	document.getElementById("count").innerHTML = library.length + " Movies Found";
+
+  	populateLibrary();
+
+}
+
+
+function populateLibrary(){
+
+	for(let i = 0;i < library.length;i++){
+
+		itemWrapper = document.createElement('div');
+		itemWrapper.classList.add('item-wrapper');
+
+		movieBlock = document.createElement('div');
+		movieBlock.classList.add("movie-item");
+		movieBlock.id = i;
+
+		if((library[i].title.includes(" ")) == false){
+			if((library[i].title).length > 9){
+				movieBlock.innerHTML = "<img src='" + library[i].image_url + "'><span class='title extended'>" + library[i].title + "</span>";
+			}
+		}else{
+			movieBlock.innerHTML = "<img src='" + library[i].image_url + "'><span class='title'>" + library[i].title + "</span>";
+		}
+		itemWrapper.appendChild(movieBlock);
+		container[0].appendChild(itemWrapper);
+	}
+
+	movieItems = document.querySelectorAll('.movie-item');
+	movieItems.forEach(element => {
+		element.addEventListener('click', function(event) {
+			const id = Number(this.id);
+			const selectedName = library[id].title;
+			localStorage.setItem("the_id", selectedName);
+			window.location.href = "details.html";
+  		});
+	});
+
+}
+
+
+function clear_filters(){
+	localStorage.setItem("the_filter", "all");
+	window.location.reload();
+}
+
+
+
+
+window.addEventListener('scroll', function() {
+	if (window.scrollY > 100) {
+		logoOffset = 60;
+  	}else{
+  		logoOffset = 100;
+  	}
+  	logo[0].style.height = logoOffset + "px";
+});
+
+
+
+
+
+
+
+
