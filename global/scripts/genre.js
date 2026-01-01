009a8e013cbfb2050b79ce805eddaab6245c912c
Troy Yauk <troyyauk@gmail.com>
2026-01-01 14:40:39 -0500

    Create genre.js

---
+++ global/scripts/genre.js
@@ -0,0 +1,30 @@
+const menuItems = document.getElementsByClassName("menu-item");
+let genre;
+
+window.onload = function() {
+
+	for(let i = 0;i < menuItems.length;i++){
+		menuItems[i].style.transition = "0.5s";
+	}
+
+	setTimeout(function() {
+		for(let i = 0;i < menuItems.length;i++){
+			setTimeout(function(){
+				menuItems[i].style.transform = "translateX(0px)";
+			}, i * 150);
+		}
+	}, 100);
+
+};
+
+
+genreItem = document.querySelectorAll('.menu-item');
+genreItem.forEach(element => {
+	element.addEventListener('click', function(event) {
+		genre = this.innerHTML;
+		genre = genre.replace(" Movies", "");
+		console.log(genre);
+		localStorage.setItem('the_filter', genre);
+		window.location.href = "../index.html";
+  	});
+});
