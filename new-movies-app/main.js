e0660a6a342b8444a4a8d956bcfff3dc476181d4
Troy Yauk <troyyauk@gmail.com>
2026-06-14 14:28:32 -0400

    Update main.js

--- new-movies-app/main.js
+++ new-movies-app/main.js
@@ -1,7 +1,9 @@
 import jsonConfig from 'https://t-yauk.github.io/peak-theatre/library.json' with {type: "json"};
 import jsonConfig2 from 'https://t-yauk.github.io/peak-theatre/lists/features.json' with {type: "json"};
+import jsonConfig3 from 'https://t-yauk.github.io/peak-theatre/directors.json' with {type: "json"};
 const data = jsonConfig.movies;
 const features = jsonConfig2.movies;
+const directors = jsonConfig3.directors;
 const pw = (document.getElementsByClassName("profile-wrapper"))[0];
 const lights = localStorage.getItem('lights');
 let featured = [];
@@ -10,18 +12,22 @@ let f = 4;
 let fK = 0;
 let m = 0;
 let k = 0;
-let action = "features";
+let s = 0;
+let action = "sidebar";
 let xVal = 0;
 let libX = 0;
-let category = "2026";
+let category;
 let catalog;
 let profType;
 let p = 0;
 let light;
 let keyVal = 0;
 let searchArray = [];
+let filter = "genre";
+let menuHTML = "2026 Movies";
 const vw = (document.getElementsByClassName("video-wrapper"))[0];
 const v = document.getElementById("video");
+let xOff = 200;
 
 window.onload = function() {
 
@@ -106,10 +112,23 @@ function populate() {
     const hero = (document.getElementsByClassName("hero"))[0];
     container.innerHTML = "";
 
-    if(category == "2026"){
-        catalog = data.filter(movies => movies.year && movies.year.includes("2026"));
-    }else{
-        catalog = data.filter(movies => movies.genre && movies.genre.includes(category));
+    if(filter == "genre"){
+        category = menuHTML.replaceAll(" Movies", "");
+        if(category == "2026"){
+            catalog = data.filter(movies => movies.year && movies.year.includes("2026"));
+        }else{
+            catalog = data.filter(movies => movies.genre && movies.genre.includes(category));
+        }
+    }else if(filter == "year"){
+        if(m == 0){
+            catalog = data.filter(movies => movies.year && movies.year.includes("2026"));
+        }else{
+            category = menuHTML.slice(0,3);
+            catalog = data.filter(movies => movies.year && movies.year.includes(category));
+        }
+    }else if(filter == "director"){
+        category = menuHTML;
+        catalog = data.filter(movies => movies.director && movies.director.includes(category));
     }
 
     for(let i=0;i<catalog.length;i++){
@@ -134,12 +153,22 @@ function populateProfile() {
         document.getElementById("director").innerHTML = "Directed By " + featured[fK].director;
         document.getElementById("description").innerHTML = featured[fK].description;
         document.getElementById("year").innerHTML = featured[fK].year;
+        if((catalog[k].genre).length > 1){
+            document.getElementById("genres").innerHTML = "<span class='genre'><span class='fa-solid fa-clock'></span> " + featured[fK].duration + "</span><span class='genre'>" + featured[fK].genre[0] + "</span><span class='genre'>" + featured[fK].genre[1] + "</span>";
+        }else{
+            document.getElementById("genres").innerHTML = "<span class='genre'>" + featured[fK].duration + "</span><span class='genre'>" + featured[fK].genre[0] + "</span>";
+        }
     }else if(profType == "library"){
         document.getElementById("image").src = catalog[k].image_url;
         document.getElementById("title").innerHTML = catalog[k].title;
         document.getElementById("director").innerHTML = "Directed By " + catalog[k].director;
         document.getElementById("description").innerHTML = catalog[k].description;
         document.getElementById("year").innerHTML = catalog[k].year;
+        if((catalog[k].genre).length > 1){
+            document.getElementById("genres").innerHTML = "<span class='genre'><span class='fa-solid fa-clock'></span> " + catalog[k].duration + "</span><span class='genre'>" + catalog[k].genre[0] + "</span><span class='genre'>" + catalog[k].genre[1] + "</span>";
+        }else{
+            document.getElementById("genres").innerHTML = "<span class='genre'>" + catalog[k].duration + "</span><span class='genre'>" + catalog[k].genre[0] + "</span>";
+        }
     }
 
     pw.classList.add("active");
@@ -215,8 +244,7 @@ function syncMenu() {
 
     container.style.transform = "translateX(" + xVal + "px)";
 
-    const menuHTML = items[m].innerHTML;
-    category = menuHTML.replaceAll(" Movies", "");
+    menuHTML = items[m].innerHTML;
     console.log(category);
     populate();
 
@@ -226,6 +254,7 @@ function syncLibrary(){
 
     const items = document.getElementsByClassName("library-item");
     const container = (document.getElementsByClassName("library"))[0];
+    const hero = (document.getElementsByClassName("hero-item"))[0];
 
     for(let i=0;i<items.length;i++){
         if(i == k){
@@ -235,6 +264,8 @@ function syncLibrary(){
         }
     }
 
+    hero.style.backgroundImage = "url('" + catalog[k].previewImage + "')";
+    hero.innerHTML = "<span class='title'>" + catalog[k].title + "<span class='year'>" + catalog[k].year + "</span></span>";
     container.style.transform = "translateX(" + libX + "px)";
 
 }
@@ -253,9 +284,24 @@ function syncKeyboard(){
 
 }
 
+function syncSidebar() {
+
+    const items = document.getElementsByClassName("sidebar-menu-item");
+
+    for(let i=0;i<items.length;i++){
+        if(i == s){
+            items[i].classList.add("active");
+        }else{
+            items[i].classList.remove("active");
+        }
+    }
+
+}
+
 function featuresListener(key){
 
     const container = (document.getElementsByClassName("featured-players-container"))[0];
+    const items = document.getElementsByClassName("featured-player");
 
     if(key === 'ArrowRight'){
         if(f > 0){
@@ -268,21 +314,39 @@ function featuresListener(key){
             f = f + 1;
             fK = fK - 1;
             syncFeatures();
+        }else{
+            action = "sidebar";
+            for(let i=0;i<items.length;i++){
+                items[i].classList.remove("active");
+            }
+            syncSidebar();
         }
     }else if(key === 'ArrowDown'){
         container.classList.remove("active");
+        for(let i=0;i<items.length;i++){
+            items[i].classList.remove("active");
+        }
         action = "menu";
         syncMenu(key);
     }else if(key === 'ArrowUp'){
         action = "awaitSearch";
         const sb = document.getElementsByClassName("searchbar");
         sb[0].style.border = "solid 5px white";
+        for(let i=0;i<items.length;i++){
+            items[i].classList.remove("active");
+        }
     }else if(key === 'Enter'){
         profType = "features";
         action = "profile";
         populateProfile();
     }else if(key === 'Backspace'){
         window.location.href = "home.html";
+    }else if(key === 'm'){
+        action = "sidebar";
+        for(let i=0;i<items.length;i++){
+            items[i].classList.remove("active");
+        }
+        syncSidebar();
     }
 
 }
@@ -291,11 +355,13 @@ function menuListener(key){
 
     const items = document.getElementsByClassName("menu-item");
     const container = (document.getElementsByClassName("library"))[0];
+    const featuredItems = document.getElementsByClassName("featured-player");
+    const hero = (document.getElementsByClassName("hero"))[0];
 
     if(key === 'ArrowRight'){
-        if(m < 18){
+        if(m < (items.length-1)){
             m = m + 1;
-            xVal = xVal - 200;
+            xVal = xVal - xOff;
             container.style.transition = "0s";
             container.style.transform = "translateX(0px)";
             syncMenu();
@@ -303,7 +369,7 @@ function menuListener(key){
     }else if(key === 'ArrowLeft'){
         if(m > 0){
             m = m - 1;
-            xVal = xVal + 200;
+            xVal = xVal + xOff;
             container.style.transition = "0s";
             container.style.transform = "translateX(0px)";
             syncMenu();
@@ -312,7 +378,11 @@ function menuListener(key){
         for(let i=0;i<items.length;i++){
             items[i].classList.remove("active");
         }
+        for(let f=0;f<featuredItems.length;f++){
+            featuredItems[f].classList.add("active");
+        }
         action = "features";
+        hero.classList.remove("active");
         syncFeatures();
     }else if(key === 'ArrowDown'){
         for(let i=0;i<items.length;i++){
@@ -323,9 +393,16 @@ function menuListener(key){
         libX = 0;
         k = 0;
         action = "movies";
+        hero.classList.add("active");
         syncLibrary();
     }else if(key === 'Backspace'){
         window.location.href = "home.html";
+    }else if(key === 'm'){
+        action = "sidebar";
+        for(let i=0;i<items.length;i++){
+            items[i].classList.remove("active");
+        }
+        syncSidebar();
     }
 
 }
@@ -364,6 +441,12 @@ function libraryListener(key){
         action = "profile";
         profType = "library";
         populateProfile();
+    }else if(key === 'm'){
+        action = "sidebar";
+        for(let i=0;i<items.length;i++){
+            items[i].classList.remove("active");
+        }
+        syncSidebar();
     }
 
 }
@@ -484,7 +567,44 @@ function videoListener(key){
 
 }
 
+function sidebarListener(key){
+
+    const items = document.getElementsByClassName("sidebar-menu-item");
+    const featuredItems = document.getElementsByClassName("featured-player");
+
+    if(key === 'ArrowRight'){
+        for(let i=0;i<items.length;i++){
+            items[i].classList.remove("active");
+        }
+        if(s == 0){
+            action = "awaitSearch";
+            const sb = document.getElementsByClassName("searchbar");
+            sb[0].style.border = "solid 5px white";
+        }else{
+            for(let x=0;x<featuredItems.length;x++){
+                featuredItems[x].classList.add("active");
+            }
+            action = "features";
+        }
+    }else if(key === 'ArrowDown'){
+        if(s < (items.length - 1)){
+            s = s + 1;
+            syncSidebar();
+        }
+    }else if(key === 'ArrowUp'){
+        if(s > 0){
+            s = s - 1;
+            syncSidebar();
+        }
+    }else if(key === 'Enter'){
+        actionSidebar();
+    }
+
+}
+
 document.addEventListener('keydown', function(event) {
+
+    const featuredItems = document.getElementsByClassName("featured-player");
 
     if(event.key === 'h'){
         window.location.href = "home.html";
@@ -500,11 +620,21 @@ document.addEventListener('keydown', function(event) {
         searchListener(event.key);
     }else if(action == "video"){
         videoListener(event.key);
+    }else if(action == "sidebar"){
+        sidebarListener(event.key);
     }else if(action == "awaitSearch"){
         if(event.key === 'ArrowDown'){
             action = "features";
             const sb = document.getElementsByClassName("searchbar");
             sb[0].style.border = "solid 5px #393e3f";
+            for(let x=0;x<featuredItems.length;x++){
+                featuredItems[x].classList.add("active");
+            }
+        }else if(event.key === 'ArrowLeft'){
+            action = "sidebar";
+            const sb = document.getElementsByClassName("searchbar");
+            sb[0].style.border = "solid 5px #393e3f";
+            syncSidebar();
         }else if(event.key === 'Enter'){
             action = "search";
             const sw = document.getElementsByClassName("search-wrapper");
@@ -513,3 +643,42 @@ document.addEventListener('keydown', function(event) {
     }
 
 });
+
+function actionSidebar() {
+
+    let the_container = (document.getElementsByClassName("categories-menu")[0]);
+    const sidebarItems = document.getElementsByClassName("sidebar-menu-item");
+    the_container.innerHTML = "";
+    xVal = 0;
+    document.getElementsByClassName("library")[0].style.transform = "translateX(0px)";
+    m = 0;
+
+    for(let i=0;i<sidebarItems.length;i++){
+        sidebarItems[i].classList.remove("active");
+    }
+
+    if(s == 1){
+        filter = "genre";
+        the_container.innerHTML = "<div class='menu-item'>2026 Movies</div><div class='menu-item'>Action Movies</div><div class='menu-item'>Adventure Movies</div><div class='menu-item'>Comedy Movies</div><div class='menu-item'>Crime Movies</div><div class='menu-item'>Documentary Movies</div><div class='menu-item'>Drama Movies</div><div class='menu-item'>Family Movies</div><div class='menu-item'>Fantasy Movies</div><div class='menu-item'>Horror Movies</div><div class='menu-item'>Musical Movies</div><div class='menu-item'>Mystery Movies</div><div class='menu-item'>Noir Movies</div><div class='menu-item'>Romance Movies</div><div class='menu-item'>Sci-Fi Movies</div><div class='menu-item'>Sport Movies</div><div class='menu-item'>Thriller Movies</div><div class='menu-item'>War Movies</div><div class='menu-item'>Western Movies</div>";
+        xOff = 200;
+        syncMenu();
+    }else if(s == 2){
+        filter = "year";
+        the_container.innerHTML = "<div class='menu-item'>2026 Movies</div><div class='menu-item'>2020 - 2026</div><div class='menu-item'>2010 - 2019</div><div class='menu-item'>2000 - 2009</div><div class='menu-item'>1990 - 1999</div><div class='menu-item'>1980 - 1989</div><div class='menu-item'>1970 - 1979</div><div class='menu-item'>1960 - 1969</div><div class='menu-item'>1950 - 1959</div><div class='menu-item'>1949 - 1940</div><div class='menu-item'>1930 - 1939</div><div class='menu-item'>1920 - 1929</div>";
+        xOff = 150;
+        syncMenu();
+    }else if(s == 3){
+        filter = "director";
+        xOff = 225;
+        for(let d=0;d<directors.length;d++){
+            const newItem = document.createElement('div');
+            newItem.classList.add("menu-item");
+            newItem.innerHTML = directors[d];
+            the_container.appendChild(newItem);
+        }
+        syncMenu();
+    }
+
+    action = "menu";
+
+}
